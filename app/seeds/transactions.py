a64d29e6e5d3a4b21b3d97e379781ca70f228540
Noah <noahwinzig@gmail.com>
2023-03-20 14:17:05 -0400

    adding some seeds to transactions to test

--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -8,8 +8,31 @@ def seed_transactions():
         amount = 30,
         completed = False
     )
-
+    demo_transaction2 = Transaction(
+        sender_id = 1,
+        recipient_id = 3,
+        payment_type = 'pay',
+        amount = 13.45,
+        completed = True
+    )
+    demo_transaction3 = Transaction(
+        sender_id = 2,
+        recipient_id = 1,
+        payment_type = 'request',
+        amount = 45.60,
+        completed = True
+    )
+    demo_transaction4 = Transaction(
+        sender_id = 3,
+        recipient_id = 2,
+        payment_type = 'request',
+        amount = 16.22,
+        completed = False
+    )
     db.session.add(demo_transaction)
+    db.session.add(demo_transaction2)
+    db.session.add(demo_transaction3)
+    db.session.add(demo_transaction4)
     db.session.commit()
 
 def undo_transactions():
