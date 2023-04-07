a08ff7da6cde86df36692d165b94db1c26942685
Noah <noahwinzig@gmail.com>
2023-04-07 12:47:04 -0400

    added some context and placeholder divs for homepage

--- react-app/src/components/home/Home.js
+++ react-app/src/components/home/Home.js
@@ -7,9 +7,22 @@ function Home(){
         <div className='homePage'>
             <div className='homeLeft'>
                 left
+                <h1>Safe, fast, & easy</h1>
+                <p>See why a couple people use Flexmo to pay, get paid, and calculate.</p>
+                <div className='barcodeHolder'>
+                    <p>
+                        Scan the code to view a mobile version of my portfolio
+                    </p>
+                    <div>
+                        barcode placeHolder
+                    </div>
+                </div>
             </div>
             <div className='homeRight'>
                 right
+                <div>
+                    image placeholder
+                </div>
             </div>
         </div>
     )
